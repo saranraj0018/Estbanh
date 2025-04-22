b9b349c44514a6eb098da1b68d994e821eca51c0
deepakpraveen <deepakpraveen2001@gmail.com>
2025-04-22 12:48:13 +0530

    notification finished

--- routes/admin.php
+++ routes/admin.php
@@ -3,6 +3,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\Auth;
 
 
 
@@ -13,11 +14,20 @@ Route::prefix('admin')
         Route::get('/', function () {
             return redirect()->route('admin.dashboard');
         });
-    
+
 
 
         Route::get('/dashboard', function () {
-            return Inertia::render('Dashboard');
+            return Inertia::render('Dashboard', [
+                'admin' => Auth::guard('admin')->user(), 
+            ]);
         })->name('admin.dashboard');
+
+        Route::post('/logout', function () {
+            Auth::guard('admin')->logout();
+            session()->invalidate();
+            session()->regenerateToken();
+            return redirect()->route('admin.login');
+        })->name('admin.logout');
     });
 
